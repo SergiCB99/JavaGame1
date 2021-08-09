dbc60da8d1518cc97b1e5b82eae3c9857e390f1d
SergiCB99 <sergicampanalesbalana@gmail.com>
2021-08-09 17:09:07 +0200

    Update JavaGame.java

--- JavaGame.java
+++ JavaGame.java
@@ -1,5 +1,7 @@
 package C4.JavaGame;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Random;
@@ -10,18 +12,24 @@ public class JavaGame extends JFrame {
 
 	private JPanel contentPane;
 	
+	//Creamos este booleano para controlar que jugador esta jugando
 	boolean jugando1 = true;
 	
+	//Creamos y inicializamos las variables para contar cuantas fichas hay
 	int contadorX=0;
 	int contadorO=0;
 	
+	//Creamos los jugadores en blanco para usarlos mas tarde
 	Player1 player1 = new Player1();
 	Player1 player2 = new Player1();
 	
+	//Creamos y inicializamos el booleano que controlara el fin de la partida
 	boolean winner = false;
 	
+	//Declaramos la classe Random para usarlo en la CPU
 	Random rand = new Random();
 	
+	//Declaramos los botones
 	static JButton BS1 = new JButton ();
 	static JButton BS2 = new JButton ();
 	static JButton BS3 = new JButton ();
@@ -40,119 +48,120 @@ public class JavaGame extends JFrame {
 	
 		setTitle("Java Game");
 		
-		setBounds(400,20,597,371);
+		setBounds(400,20,459,271);
 	
 		contentPane = new JPanel();
-		
+		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		setContentPane(contentPane);
-		
-		JButton nueva_partida = new JButton ("Nueva Partida");
-		nueva_partida.setBounds(371, 11, 139, 32);
-		contentPane.add(nueva_partida);
-		
-		JLabel estado = new JLabel ();
-		estado.setHorizontalAlignment(SwingConstants.CENTER);
-		estado.setBounds(296, 54, 214, 35);
-		contentPane.add(estado);
-		
-		JLabel J1 = new JLabel ("Jugador 1");
-		J1.setHorizontalAlignment(SwingConstants.CENTER);
-		J1.setBounds(296, 100, 129, 35);
-		contentPane.add(J1);
-		
-		JLabel J2 = new JLabel ("Jugador 2");
-		J2.setHorizontalAlignment(SwingConstants.CENTER);
-		J2.setBounds(296, 175, 129, 35);
-		contentPane.add(J2);
-		
+		//Panel derecho
 		JLabel N1 = new JLabel ("Nombre:");
-		N1.setHorizontalAlignment(SwingConstants.CENTER);
-		N1.setBounds(333, 129, 92, 35);
+		N1.setBounds(229, 98, 54, 14);
+		N1.setHorizontalAlignment(SwingConstants.LEFT);
 		contentPane.add(N1);
 		
-		JLabel N2 = new JLabel ("Nombre:");
-		N2.setHorizontalAlignment(SwingConstants.CENTER);
-		N2.setBounds(333, 221, 92, 35);
-		contentPane.add(N2);
+		JLabel J1 = new JLabel ("Jugador 1");
+		J1.setBounds(301, 73, 65, 14);
+		J1.setHorizontalAlignment(SwingConstants.LEFT);
+		contentPane.add(J1);
 		
 		JTextField TF1 = new JTextField();
-		TF1.setBounds(439, 132, 132, 29);
+		TF1.setBounds(293, 95, 137, 20);
 		contentPane.add(TF1);
 		
+		JLabel J2 = new JLabel ("Jugador 2");
+		J2.setBounds(301, 137, 67, 14);
+		J2.setHorizontalAlignment(SwingConstants.LEFT);
+		contentPane.add(J2);
+		
+		JLabel N2 = new JLabel ("Nombre:");
+		N2.setBounds(229, 165, 54, 14);
+		N2.setHorizontalAlignment(SwingConstants.LEFT);
+		contentPane.add(N2);
+		
 		JTextField TF2 = new JTextField();
-		TF2.setBounds(439, 224, 132, 29);
+		TF2.setBounds(293, 162, 137, 20);
 		contentPane.add(TF2);
 		
 		JLabel tipo2 = new JLabel ("Tipo:");
-		tipo2.setHorizontalAlignment(SwingConstants.CENTER);
-		tipo2.setBounds(333, 286, 73, 35);
+		tipo2.setBounds(229, 193, 35, 20);
+		tipo2.setHorizontalAlignment(SwingConstants.LEFT);
 		contentPane.add(tipo2);
 		
-		JRadioButton H2 = new JRadioButton("Humano",false);
-		H2.setBounds(427, 286, 73, 35);
-		contentPane.add(H2);
+		ButtonGroup BG2 = new ButtonGroup();
 		
 		JRadioButton C2 = new JRadioButton("CPU",true);
-		C2.setBounds(498, 285, 73, 40);
+		C2.setBounds(375, 196, 62, 14);
 		contentPane.add(C2);
-		
-		ButtonGroup BG2 = new ButtonGroup();
-		BG2.add(H2);
 		BG2.add(C2);
 		
-		BS1.setBounds(0, 11, 72, 71);
+		JRadioButton H2 = new JRadioButton("Humano",false);
+		H2.setBounds(293, 196, 73, 14);
+		contentPane.add(H2);
+		BG2.add(H2);
+		
+		JButton nueva_partida = new JButton ("Nueva Partida");
+		nueva_partida.setBounds(229, 11, 123, 27);
+		contentPane.add(nueva_partida);
+		
+		JLabel estado = new JLabel ();
+		estado.setBounds(229, 43, 191, 30);
+		estado.setForeground(Color.RED);
+		estado.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		estado.setHorizontalAlignment(SwingConstants.LEFT);
+		contentPane.add(estado);
+		
+		//TABLERO
+		
+		BS1.setBounds(10, 11, 67, 62);
 		BS1.setEnabled(false);
 		contentPane.add(BS1);
-		
-		BS2.setBounds(80, 11, 72, 71);
+		BS2.setBounds(81, 11, 67, 62);
 		BS2.setEnabled(false);
 		contentPane.add(BS2);
-		
-		BS3.setBounds(162, 11, 72, 71);
+		BS3.setBounds(152, 11, 67, 62);
 		BS3.setEnabled(false);
 		contentPane.add(BS3);
-		
-		BS4.setBounds(0, 93, 72, 71);
+		BS4.setBounds(10, 78, 67, 66);
 		BS4.setEnabled(false);
 		contentPane.add(BS4);
-		
-		BS5.setBounds(80, 93, 72, 71);
+		BS5.setBounds(81, 78, 67, 66);
 		BS5.setEnabled(false);
 		contentPane.add(BS5);
-	
-		BS6.setBounds(162, 93, 72, 71);
+		BS6.setBounds(152, 78, 67, 66);
 		BS6.setEnabled(false);
 		contentPane.add(BS6);
-	
-		BS7.setBounds(0, 175, 72, 71);
+		BS7.setBounds(10, 149, 67, 65);
 		BS7.setEnabled(false);
 		contentPane.add(BS7);
-		
-		BS8.setBounds(80, 175, 72, 71);
+		BS8.setBounds(81, 149, 67, 65);
 		BS8.setEnabled(false);
 		contentPane.add(BS8);
-		
-		BS9.setBounds(162, 175, 72, 71);
+		BS9.setBounds(152, 149, 67, 65);
 		BS9.setEnabled(false);
 		contentPane.add(BS9);
-		
+
+		//Acciones
 		ActionListener partida_nueva = new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
+				//Variables para guardar el nombre y si es humano o CPU 
 				String nombre1 = TF1.getText();
 				String nombre2 = TF2.getText();
 				boolean isCPU1 = false;
 				boolean isCPU2 = false;
+				
+				//Canviamos el valor de winner a false
 				winner = false;	
 				
-					if(H2.isSelected()) {
-						isCPU2 = false;
-					}else if(C2.isSelected()){
-						isCPU2 = true;
-					}
+				//Guardamos el tipo del jugador 2
+				if(H2.isSelected()) {
+					isCPU2 = false;
+				}else {
+					isCPU2 = true;
+				}
 		
+				//If's para validar que todos los datos de los jugadores estan rellenados
 				if(nombre1.length()==0) {
 					JOptionPane.showMessageDialog(null,"Introduce el nombre del jugador 1");
 				}else if(nombre2.length()==0) {
@@ -161,43 +170,41 @@ public class JavaGame extends JFrame {
 					JOptionPane.showMessageDialog(null,"Selecciona que es el jugador 2");
 				}else {	
 				
-					player1 = new Player1(nombre1,isCPU1);
-					player2 = new Player1(nombre2,isCPU2);
+				//Llenamos los dos jugadores
+				player1 = new Player1(nombre1,isCPU1);
+				player2 = new Player1(nombre2,isCPU2);
 					
-					//System.out.println(player2.toString());
+				//Vaciamos la label que nos indicara el ganador
+				estado.setText("");
 					
-					estado.setText("");
+				//Activamos los botones
+				BS1.setEnabled(true);
+				BS2.setEnabled(true);
+				BS3.setEnabled(true);
+				BS4.setEnabled(true);
+				BS5.setEnabled(true);
+				BS6.setEnabled(true);
+				BS7.setEnabled(true);
+				BS8.setEnabled(true);
+				BS9.setEnabled(true);
 					
-					BS1.setEnabled(true);
-					BS2.setEnabled(true);
-					BS3.setEnabled(true);
-					BS4.setEnabled(true);
-					BS5.setEnabled(true);
-					BS6.setEnabled(true);
-					BS7.setEnabled(true);
-					BS8.setEnabled(true);
-					BS9.setEnabled(true);
+				//Los ponemos en blanco
+				BS1.setText("");
+				BS2.setText("");
+				BS3.setText("");
+				BS4.setText("");
+				BS5.setText("");
+				BS6.setText("");
+				BS7.setText("");
+				BS8.setText("");
+				BS9.setText("");
 					
-					BS1.setText("");
-					BS2.setText("");
-					BS3.setText("");
-					BS4.setText("");
-					BS5.setText("");
-					BS6.setText("");
-					BS7.setText("");
-					BS8.setText("");
-					BS9.setText("");
-					
-					jugando1 = true;
-					contadorX=0;
-					contadorO=0;
-					
-					/*while(!winner&& !jugando1 && player2.isCPU) {
-						clicarCPU();
-					}*/
-					
-				}
+				//Canviamos el valor de las variables para hacer comprovaciones en otras funciones
+				jugando1 = true;
+				contadorX=0;
+				contadorO=0;
 				
+				}	
 			}
 		};
 		
@@ -221,16 +228,16 @@ public class JavaGame extends JFrame {
 					O=true;
 				}
 				
-				if(contadorX<3 || contadorO<3) {//Si se cumple la condicion entrara en este primer if que pone valor a un boton vacio dependiendo del jugador que este jugando
-					if(BS1 == source && jugando1 && en_blanco) {//Boton 1
-						BS1.setText("X");
-						jugando1=false;
-						contadorX++;
-						clicarCPU();
-					}else if(BS1 == source && !jugando1 && en_blanco) {
-							BS1.setText("O");
-							jugando1=true;
-							contadorO++;
+				if(contadorX<3 || contadorO<3) {//Si el jugador tiene menos de tres fichas modifica el valor de un boton en blanco
+					if(BS1 == source && jugando1 && en_blanco) {//Boton 1 turno jugador 1
+						BS1.setText("X");//Canvia el texto del boton a X
+						jugando1=false;//Canvia el valor del booleano a false para "canviar de turno"
+						contadorX++;//Sumamos uno al contador de X
+						clicarCPU();//Funcion que simula la CPU
+					}else if(BS1 == source && !jugando1 && en_blanco) {//Boton 1 turno jugador 2
+							BS1.setText("O");//Canvia el texto del boton a O
+							jugando1=true;//Canvia el valor del booleano a true para "canviar de turno"
+							contadorO++;//Sumamos uno al contador de X
 					}else if(BS2 == source && jugando1 && en_blanco) {//Boton 2
 						BS2.setText("X");
 						jugando1=false;
@@ -304,14 +311,14 @@ public class JavaGame extends JFrame {
 							jugando1=true;
 							contadorO++;
 					}
-				}else {//Si ya hay tres X o tres O entraria en este else y pondria un boton en blanco
-					if(BS1 == source && jugando1 && X) {//Boton 1
-						BS1.setText("");
-						contadorX--;
-					}else if(BS1 == source && !jugando1 && O) {
-						BS1.setText("");
-						contadorO--;
-						clicarCPU();
+				}else {//Si ya hay tres X o tres O entraria en este else
+					if(BS1 == source && jugando1 && X) {//Boton 1 turno jugador 1
+						BS1.setText("");//Canvia el texto de boton a vacio
+						contadorX--;//Restamos uno a el contador de X
+					}else if(BS1 == source && !jugando1 && O) {//Boton 1 turno jugador 2
+						BS1.setText("");////Canvia el texto de boton a vacio
+						contadorO--;//Restamos uno a el contador de X
+						clicarCPU();//Se llama a la funcion clicarCPU para poner la O
 					}else if(BS2 == source && jugando1 && X) {//Boton 2
 						BS2.setText("");
 						contadorX--;
@@ -373,22 +380,25 @@ public class JavaGame extends JFrame {
 				
 				//Check si hay una combinacion ganadora en el tablero despues del ultimo movimiento
 				if (!winner) {
-					 if ((BS1.getText().equals("X") || BS1.getText().equals("O")) &&BS1.getText().equals(BS2.getText()) && BS2.getText().equals(BS3.getText())) winner=true;
-					    if ((BS4.getText().equals("X") || BS4.getText().equals("O")) &&BS4.getText().equals(BS5.getText()) && BS5.getText().equals(BS6.getText())) winner=true;
-					    if ((BS7.getText().equals("X") || BS7.getText().equals("O")) &&BS7.getText().equals(BS8.getText()) && BS8.getText().equals(BS9.getText())) winner=true;
-					    //check columns
-					    if ((BS1.getText().equals("X") || BS1.getText().equals("O")) &&BS1.getText().equals(BS4.getText()) && BS4.getText().equals(BS7.getText())) winner=true;
-					    if ((BS2.getText().equals("X") || BS2.getText().equals("O")) &&BS2.getText().equals(BS5.getText()) && BS5.getText().equals(BS8.getText())) winner=true;
-					    if ((BS3.getText().equals("X") || BS3.getText().equals("O")) &&BS3.getText().equals(BS6.getText()) && BS6.getText().equals(BS9.getText())) winner=true;
-					    //check diagonal
-					    if ((BS1.getText().equals("X") || BS1.getText().equals("O")) &&BS1.getText().equals(BS5.getText()) && BS5.getText().equals(BS9.getText())) winner=true;
-					    if ((BS3.getText().equals("X") || BS3.getText().equals("O")) &&BS3.getText().equals(BS5.getText()) && BS5.getText().equals(BS7.getText())) winner=true;
+					//check rows
+					if ((BS1.getText().equals("X") || BS1.getText().equals("O")) &&BS1.getText().equals(BS2.getText()) && BS2.getText().equals(BS3.getText())) winner=true;
+					if ((BS4.getText().equals("X") || BS4.getText().equals("O")) &&BS4.getText().equals(BS5.getText()) && BS5.getText().equals(BS6.getText())) winner=true;
+					if ((BS7.getText().equals("X") || BS7.getText().equals("O")) &&BS7.getText().equals(BS8.getText()) && BS8.getText().equals(BS9.getText())) winner=true;
+					//check columns
+					if ((BS1.getText().equals("X") || BS1.getText().equals("O")) &&BS1.getText().equals(BS4.getText()) && BS4.getText().equals(BS7.getText())) winner=true;
+					if ((BS2.getText().equals("X") || BS2.getText().equals("O")) &&BS2.getText().equals(BS5.getText()) && BS5.getText().equals(BS8.getText())) winner=true;
+					if ((BS3.getText().equals("X") || BS3.getText().equals("O")) &&BS3.getText().equals(BS6.getText()) && BS6.getText().equals(BS9.getText())) winner=true;
+					//check diagonal
+					if ((BS1.getText().equals("X") || BS1.getText().equals("O")) &&BS1.getText().equals(BS5.getText()) && BS5.getText().equals(BS9.getText())) winner=true;
+					if ((BS3.getText().equals("X") || BS3.getText().equals("O")) &&BS3.getText().equals(BS5.getText()) && BS5.getText().equals(BS7.getText())) winner=true;
 				}
+				//Dependiendo de el valor del ultimo boton clicado el label mostrara un texto u otro
 				if (source.getText().equals("X") && winner) {
 					estado.setText(player1.nombre+" gana");
 				} else if (source.getText().equals("O") && winner) {
 					estado.setText(player2.nombre+" gana");
 				}
+				//Si gana se desabilitan los botones
 				if (winner) {
 					BS1.setEnabled(false);
 					BS2.setEnabled(false);
@@ -404,6 +414,7 @@ public class JavaGame extends JFrame {
 			}
 		};
 		
+		//Asignamos el ActionListener a los botones
 		nueva_partida.addActionListener(partida_nueva);
 		BS1.addActionListener(pulsarBoton);
 		BS2.addActionListener(pulsarBoton);
@@ -420,16 +431,22 @@ public class JavaGame extends JFrame {
 	}
 
 	public void clicarCPU() {
+		//Si el jugador 2 es CPU entrara al if
 		if(player2.isCPU) {
+			//Generamos un numero aleatorio entre 1 y 9
 			int r = rand.nextInt((9 - 1) + 1) + 1;
-				
-			System.out.println("Tirada CPU "+r);
 			
+				//Dependiendio del numero generado pulsara un boton u otro
 				switch(r) {
 					case 1:
+						/*
+						  	Controlamos donde pondra la ficha de la siguiente manera
+							Si tiene menos de 3 fichas colocadas la tiene que poner en algun boton que este en blanco
+							Y si tiene tres tiene que clicar en una O para quitar-la y volver a poner-la
+						*/
 						if(contadorO<3 && BS1.getText()==""||contadorO==3 && BS1.getText()=="O") {
 							BS1.doClick();
-						}else {
+						}else {	//Sino se cumple ninguna de estas vuelve a llamar a la funcion para generar otro numero
 							clicarCPU();
 						}
 					break;
@@ -490,7 +507,6 @@ public class JavaGame extends JFrame {
 						}
 					break;
 				}
-	
 		}
 	}
 }
